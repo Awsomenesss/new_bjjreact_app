5405a8ee94a1da335deac393dd523183f0ed7cdc
Awsomenesss <>
2023-10-26 00:05:09 +0000

    implimented the the sign up page

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+
+axios.defaults.baseURL ='https://pp5-bjj-api-2269f4220822.herokuapp.com/';
+axios.defaults.headers.post["Content-Type"] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
